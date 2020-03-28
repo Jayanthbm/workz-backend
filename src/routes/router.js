1e2aebdce0a28c8f50dcfe7694cfd6b0b092d09a
jayanth bharadwaj <jayanthbharadwaj@jayanths-MacBook-Air.local>
2020-03-28 17:03:33 +0530

    Updated router

--- src/routes/router.js
+++ src/routes/router.js
@@ -3,27 +3,83 @@ const db = require("../models/db");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
 const bcrypt = require('bcryptjs');
+const CC = require('../constants');
 
+//Middlewares
+
+const auth = require('../middlwares/auth');
 router.get("/", async (req, res) => {
     res.send({
         message: "Hello"
     })
 })
 
-router.get("/login", async (req, res) => {
-    let password = "123456";
-    let hashedpassword = `$2a$10$IR82u9x1Z.ivKPZcT8Bp1eGRh6uhBk87eg6OOXFbf01JAhLINmODK`;
-    bcrypt.compare(password, hashedpassword, function (err, res1) {
-        if (res1) {
-            res.send({
-                message: "Passwords Match"
-            })
-        } else {
-            res.send({
-                message: "Passwords Doesn't Match"
-            })
-        }
-    })
+router.post("/login", async (req, res) => {
+    let username = req.body.username;
+    let password = req.body.password;
+    const sql = `SELECT managerId,username,email,password,password_updated
+                FROM manager
+                WHERE username ='${username}' or email ='${username}'`;
+    let {
+        results
+    } = await db.query(sql);
+    if (Object.keys(results).length === 0) {
+        res.send({
+            message: "Invalid Credentials"
+        });
+    } else {
+        let haspass = results[0].password;
+        let id = results[0].managerId;
+        let password_updated = results[0].password_updated;
+        bcrypt.compare(password, haspass, function (err, result) {
+            if (result) {
+                const token = jwt.sign({
+                    managerid: id
+                }, CC.SECRET_KEY, {
+                    expiresIn: '3h'
+                });
+                res.send({
+                    token,
+                    password_updated
+                })
+            } else {
+                res.send({
+                    message: "Invalid Credentials"
+                });
+            }
+        })
+    }
 })
 
+router.post("/updatepass", auth, async (req, res) => {
+    let managerId = req.managerid;
+    let password = req.body.password;
+    if (password) {
+        bcrypt.hash(password, 10, async function (err, hash) {
+            console.log(hash);
+
+            if (hash) {
+                const sql = `UPDATE manager SET password='${hash}',password_updated = 1
+            WHERE managerId = ${managerId}`;
+                let {
+                    results
+                } = await db.query(sql);
+                if (Object.keys(results).length === 0) {
+                    res.send({
+                        message: "Error Updating Password"
+                    });
+                } else {
+                    res.send({
+                        message: "Password Updated Succesfully"
+                    })
+                }
+            }
+        });
+    } else {
+        res.send({
+            message: "Please Provide Password"
+        })
+    }
+
+})
 module.exports = router;
