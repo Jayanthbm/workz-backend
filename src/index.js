9d0a665cef4aa2e00309fa5b209e365856da100d
jayanth bharadwaj <jayanthbharadwaj@jayanths-MacBook-Air.local>
2020-03-29 15:08:05 +0530

    minor change

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const routes = require('./routes/router');
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 app.use(routes);
 app.listen(4000, () => {
